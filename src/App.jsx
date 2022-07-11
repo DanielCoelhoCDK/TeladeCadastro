ffeb987229e2aa1a7a29f6f96beded85ff1a9379
Daniel Coelho <danielcoelho.cdk@gmail.com>
2022-07-11 00:04:32 -0300

    Alteração dos dados cadastrais

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         bg="teal.500"
         color="white"
         fontWeight="bold"
-        fontSizae="4x1"
+        fontSize="25px"
         pb="8"
       >
         CADASTRO DE ALUNO
@@ -41,7 +41,7 @@ function App() {
           position="absolute"
           borderRadius={5}
           p="6"
-          boxShadow="0 2px 2px #ccc"
+          boxShadow="0 8px 8px #ccc"
         >
           <FormControl display="flex" flexDir="column" gap="4">
             <HStack spacing="4">
@@ -60,7 +60,7 @@ function App() {
                 <Input id="nasc" type="date" />
               </Box>
               <Box w="100%">
-                <FormLabel htmlFor="natural">Naturalidade</FormLabel>
+                <FormLabel htmlFor="natural">Cônjuge</FormLabel>
                 <Input id="natural" />
               </Box>
             </HStack>
@@ -75,13 +75,43 @@ function App() {
               </Box>
             </HStack>
             <HStack spacing="4">
-              <Box w="100%">
+            <Box w="100%">
                 <FormLabel htmlFor="endereco">Endereço</FormLabel>
                 <Input id="endereco" />
               </Box>
               <Box w="100%">
                 <FormLabel htmlFor="cidade">Cidade</FormLabel>
                 <Input id="cidade" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="20%">
+                <FormLabel htmlFor="uf">UF</FormLabel>
+                <Input id="uf" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="complemento">Complemento</FormLabel>
+                <Input id="complemento" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="nomepai">Nome do Pai</FormLabel>
+                <Input id="nomepai" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="nomemae">Nome da Mãe</FormLabel>
+                <Input id="nomemae" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="classe">Classe</FormLabel>
+                <Input id="classe" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="datadamatricula">Data da Matrícula</FormLabel>
+                <Input id="datadamatricula" type="date" />
               </Box>
             </HStack>
             <HStack spacing="4">
